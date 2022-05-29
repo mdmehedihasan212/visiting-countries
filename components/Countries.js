55020d072e1bac6da7fe8e1a08215fa05773a344
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-29 19:37:04 +0600

    add search input for search country

--- components/Countries.js
+++ components/Countries.js
@@ -1,22 +1,36 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, TextInput } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import Country from './Country';
 
 export default function Countries() {
     const [countries, setCountries] = useState([]);
+    const [searched, setsearched] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v2/all')
             .then(res => res.json())
-            .then(data => setCountries(data))
+            .then(data => {
+                setsearched(data)
+                setCountries(data)
+            })
     }, [])
+
+    const handleToSerarch = text => {
+        const filtered = countries.filter(country => country.name.includes(text))
+        setsearched(filtered);
+    }
 
     return (
         <View>
-            <Text>Countries: {countries.length}</Text>
+            <Text style={styles.header}>Countries: {searched.length}</Text>
+            <TextInput
+                onChangeText={handleToSerarch}
+                style={styles.input}
+                placeholder="Search your country..."
+            ></TextInput>
             <ScrollView>
                 {
-                    countries.map(country => <Country
+                    searched.map(country => <Country
                         key={country.id}
                         country={country}
                     ></Country>)
@@ -25,3 +39,17 @@ export default function Countries() {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    header: {
+        marginTop: 50,
+        fontSize: 20,
+        color: 'blue'
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    }
+})
